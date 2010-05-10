c6f7f7539ca9f22ebc1c764db15b4fd97219f5e1
Kien Tran <kientran@kientran.com>
2010-05-10 04:02:14 -0500

    Fixed Navigation

--- js/slider.js
+++ js/slider.js
@@ -6,4 +6,12 @@ $(document).ready(function() {
       }, function() {
       $(".cover", this).stop().animate({top:'140px'},{queue:false,duration:160});
     });
+    $("ul.sf-menu").supersubs({ 
+          minWidth:    12,   // minimum width of sub-menus in em units 
+          maxWidth:    27,   // maximum width of sub-menus in em units 
+          extraWidth:  1     // extra width can ensure lines don't sometimes turn over 
+                               // due to slight rounding differences and font-family 
+      }).superfish();  // call supersubs first, then superfish, so that subs are 
+                      // not display:none when measuring. Call before initialising 
+                      // containing tabs for same reason. 
 });
