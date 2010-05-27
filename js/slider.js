0f61b1a31e6f501d512c11fdc59161da45b14bf3
Kien Tran <kientran@kientran.com>
2010-05-27 17:06:24 -0500

    Fix menu

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,11 @@
 	//To switch directions up/down and left/right just place a "-" in front of the top/left attribute
 $(document).ready(function() {
     //Caption Sliding (Partially Hidden to Visible)
-    $('.boxgrid.caption').hover(function(){
+/*    $('.boxgrid.caption').hover(function(){
       $(".cover", this).stop().animate({top:'75px'},{queue:false,duration:160});
       }, function() {
       $(".cover", this).stop().animate({top:'140px'},{queue:false,duration:160});
-    });
+    });*/
     $("ul.sf-menu").supersubs({ 
           minWidth:    12,   // minimum width of sub-menus in em units 
           maxWidth:    27,   // maximum width of sub-menus in em units 
