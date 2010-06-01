b62038c7fcaa91d34da260eec6e5d7cbe6fb9bfc
Kien Tran <kientran@kientran.com>
2010-06-01 00:58:33 -0500

    CSS cleanup and tweaks

--- js/slider.js
+++ js/slider.js
@@ -1,26 +1,37 @@
 	//To switch directions up/down and left/right just place a "-" in front of the top/left attribute
 $(document).ready(function() {
-    //Caption Sliding (Partially Hidden to Visible)
-/*    $('.boxgrid.caption').hover(function(){
-      $(".cover", this).stop().animate({top:'75px'},{queue:false,duration:160});
-      }, function() {
-      $(".cover", this).stop().animate({top:'140px'},{queue:false,duration:160});
-    });*/
-    $("ul.sf-menu").supersubs({ 
-          minWidth:    12,   // minimum width of sub-menus in em units 
-          maxWidth:    27,   // maximum width of sub-menus in em units 
-          extraWidth:  1     // extra width can ensure lines don't sometimes turn over 
-                               // due to slight rounding differences and font-family 
-      }).superfish();  // call supersubs first, then superfish, so that subs are 
+  $("ul.sf-menu").supersubs({ 
+    minWidth:    12,  // minimum width of sub-menus in em units 
+    maxWidth:    27,  // maximum width of sub-menus in em units 
+    extraWidth:  1    // extra width can ensure lines don't sometimes turn over 
+                      // due to slight rounding differences and font-family 
+  }).superfish();     // call supersubs first, then superfish, so that subs are 
                       // not display:none when measuring. Call before initialising 
                       // containing tabs for same reason. 
-    $("#upper_front").after('<div id="pager">').cycle({
-      fx: 'fade',
-      timeout: 7000,
-      pause: true,
-      pager: '#pager',
-      pagerAnchorBuilder: function(idx, slide) {
-        return '<a href="#">' + (idx + 1).toString() + '</a>';
+  $("#upper_front").after('<div id="pager">').cycle({
+    fx: 'fade',
+    timeout: 7000,
+    pause: true,
+    pager: '#pager',
+    pagerAnchorBuilder: function(idx, slide) {
+      return '<a href="#">' + (idx + 1).toString() + '</a>';
+    }
+  });
+
+  $.fn.search = function() {
+    return this.focus(function() {
+      if( this.value == this.defaultValue ) {
+        this.value = "";
+        $(this).toggleClass('faded');
+      }
+    }).blur(function() {
+      if( !this.value.length ) {
+        this.value = this.defaultValue;
+        $(this).toggleClass('faded');
       }
     });
+  };
+
+  $("#searchfield").search();
+    
 });
