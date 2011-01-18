3399355b20766f64f4d6b475f5ee8d31c44316b3
Kien Tran <kien@kientran.com>
2011-01-18 17:14:36 -0600

    Tweak sidebar to show child pages correctly

--- sidebar.php
+++ sidebar.php
@@ -2,51 +2,46 @@
   <div id="sidebar_container"><?php
 
 if (is_front_page() ) {
-?>
+   echo"<h2>Who We Are</h2>";
+   echo"<p>Catholic Charities serves the 28 counties in the Diocese of Fort Worth. We provide services to individuals, families and children among us in need as we advocate compassion and justice in our community.</p>";
 
-    <h2>Who We Are</h2>
-    <p>Catholic Charities serves the 28 counties in the Diocese of Fort Worth. We provide services to individuals, families and children among us in need as we advocate compassion and justice in our community.</p>
-<?php
 } else {
-  if($post->post_parent) {
-    $children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0&depth=1");
-    $titlenamer = get_the_title($post->post_parent);
-    $titleurl = get_permalink($post->post_parent);
+  
+  echo '<ul>';
+  $output = wp_list_pages('echo=0&depth=1&title_li=<h2>Top Level Pages</h2>' );
+  if (is_page()) {
+    $page = $post->ID;
+    if ($post->post_parent) {
+      $page = $post->post_parent;
+    }
+    $children=wp_list_pages( 'echo=0&child_of=' . $page . '&title_li=' );
+    if ($children) {
+      $output = wp_list_pages ('echo=0&child_of=' . $page . '&title_li=<h2>' . get_the_title($post->ID) . '</h2>');
+    }
   }
-  else {
-    $children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0&depth=1");
-    $titlenamer = get_the_title($post->ID);
-    $titleurl = get_permalink($post->ID);
-  }
-  if ($children) { ?>
+  echo $output;
+  echo '</ul>';
+} 
+  
+  
+if ( get_post_type() == 'job-posting') :
 
-    <h2><a href='<?php echo $titleurl ?>'><? echo $titlenamer ?></a></h2>
-      <ul>
-      <?php echo $children; ?>
-      </ul>
-      
-    
-    
-<?php
-  } 
-  if ( get_post_type() == 'job-posting') :
-?>
-  <h2><a href='/employment'>Employment Opportunities</h2>
-<?php
-$loop = new WP_Query( array( 'post_type' => 'job-posting', 'posts_per_page' => 10 ) );?>
-<ul>
-<?php
-while ( $loop->have_posts() ) : $loop->the_post();
-?>
+  echo "<h2><a href='/employment'>Employment Opportunities</h2>";
+  $loop = new WP_Query( array( 'post_type' => 'job-posting', 'posts_per_page' => 10 ) );
+  
+  echo "<ul>";
+
+  while ( $loop->have_posts() ) : $loop->the_post();
+  ?>
 	<li ><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></li>
 <?php
 endwhile;
 ?>
+
 </ul>
 <?php
 
   endif;
-}
 ?>
   <?php
         $args = array(
