483a864cea9b1648e64c2f6d7bd0677b47c43c55
Kien Tran <kien@kientran.com>
2011-02-09 18:10:59 -0600

    Using an iframe for the form

--- page-newsletter-signup.php
+++ page-newsletter-signup.php
@@ -5,26 +5,19 @@ Template Name: Newsletter Signup Form
 ?>
 <?php get_header() ?>
 
-
 <div id="content">
 <div id="content_container">
-
-
 
 		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 
-
 			<div class="entry-content">
-Content
-
+				<iframe src='/wp-content/themes/ccdofw.org/newsletter-signup.php' width='100%' height='100%'>
+				</iframe>
 			</div><!-- .entry-content -->	
-
 		</div>
 </div>
 </div>
-
-
 
 <?php get_sidebar() ?>
 
