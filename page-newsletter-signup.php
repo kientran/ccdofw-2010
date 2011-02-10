cef6e100c8eabb96dc09a2a45249d51de5c380e6
Kien Tran <kien@kientran.com>
2011-02-09 18:34:16 -0600

    Revert "Dynamic resize of iFrame"
    This reverts commit dab8ebb621665fb58489245b7393f02adfbc4d74.

--- page-newsletter-signup.php
+++ page-newsletter-signup.php
@@ -7,19 +7,12 @@ Template Name: Newsletter Signup Form
 
 <div id="content">
 <div id="content_container">
-<script type='text/javascript'>
-<!--
-function resizeIframe(newHeight)
-{
-  document.getElementById('iFrame').style.height = parseInt(newHeight) + 100 + 'px';
-}
-// ->
-</script>
+
 		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 
 			<div class="entry-content">
-				<iframe id='iFrame' src='/wp-content/themes/ccdofw.org/newsletter-signup.php' width='100%'>
+				<iframe src='/wp-content/themes/ccdofw.org/newsletter-signup.php' width='100%' height='100%'>
 				</iframe>
 			</div><!-- .entry-content -->	
 		</div>
