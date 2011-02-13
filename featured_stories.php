c048ce79d74ac2eada5f73635f4316d35676a5ed
Kien Tran <kien@kientran.com>
2011-02-13 16:19:35 -0600

    Add get help now button, hide volunteer checkin

--- featured_stories.php
+++ featured_stories.php
@@ -48,8 +48,9 @@ if( $featuredpages ) {
 ?>
 
 <div id="action_buttons">
+<a href="http://dev.catholiccharitiesfortworth.org/programs/" class="blue awesome">Get Help Now</a>
 <a href="https://app.etapestry.com/hosted/CatholicCharitiesFortWorth/OnlineDonation.html" class="green awesome">Donate Now</a>
-<a href="#" class="blue awesome">Volunteer Check-in</a>
+<!--<a href="#" class="blue awesome">Volunteer Check-in</a>-->
 <a href="/media/publications/newsletter-signup/" class="blue awesome">Newsletter Signup</a>
 <a href="http://dev.catholiccharitiesfortworth.org/about/employment/" class="blue awesome">Careers</a>
 
